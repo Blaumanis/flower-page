b4442d5409548539356edd61ef9fb4ec6c299028
Blaumanis <blaumanis.alberts@gmail.com>
2022-10-26 18:56:56 +0300

    first commit

--- src/customHook/useFetch.js
+++ src/customHook/useFetch.js
@@ -9,7 +9,7 @@ export const useFetch = (url) => {
         headers: {
             "Content-Type": "application/json",
         },
-        mode: "no-cors",
+        // mode: "no-cors",
         method: "GET"
     })
 
