c2fa3e9a8878e2396c57ae0acfe4814af8db3e91
Blaumanis <blaumanis.alberts@gmail.com>
2022-10-26 18:33:13 +0300

    first commit

--- src/customHook/useFetch.js
+++ src/customHook/useFetch.js
@@ -1,20 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
 export const useFetch = (url) => {
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
 
-    const getData = async() => {
-        const response = await fetch(url);
-        const data = await response.json();
-        setData(data)
-        setLoading(false);
-    }
+  const getData = async () => {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    })
+    const data = await response.json()
+    setData(data)
+    setLoading(false)
+  }
 
-    useEffect(() => {
-        getData();
-    },[url])
-    return {loading,data}
+  useEffect(() => {
+    getData()
+  }, [url])
+  return { loading, data }
 }
 
-export default useFetch;
+export default useFetch
